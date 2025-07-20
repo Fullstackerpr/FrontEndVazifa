3dd11b99269df6f1dde1d2dd043e8c155d31b2be
Fullstackerpr <bahodirnabijanov782@gmail.com>
2025-07-20 10:13:53 +0500

    soat and fake chat

--- js/chat.js
+++ js/chat.js
@@ -1,23 +1,45 @@
 import { DATA } from "./data.js";
-const contentE1 = document.querySelector(".content");
-const inputE1 = document.querySelector(".input");
-const formE1 = document.querySelector(".form");
 
-formE1.addEventListener("submit", (event) => {
+const contentEl = document.querySelector(".content");
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".input");
+
+const getTimeFormat = () => {
+  let date = new Date();
+  return `${date.getHours()}:${date.getMinutes()}`;
+};
+
+const randomMessage = () => {
+  let index = Math.floor(Math.random() * DATA.length);
+  const divEl = document.createElement("div");
+  divEl.className = "msg";
+
+  divEl.innerHTML = `
+    <p>${DATA[index]}</p>
+    <span>${getTimeFormat()}</span>
+  `;
+
+  contentEl.appendChild(divEl);
+};
+
+formEl.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const value = inputE1.value;
+  const value = inputEl.value;
 
-  if(!value.trim()) return null;
+  if (!value.trim()) return null;
 
-  const div1 = document.createElement("div");
-  div1.className = "msg me";
+  const divEl = document.createElement("div");
+  divEl.className = "msg me"
+  divEl.innerHTML = `
+  <p>${value}</p>
+  <span>${getTimeFormat()}</span>
+  `;
 
-  div1.innerHTML = `
-            <p>${value}</p>
-            <span>9:54</span>
-        `;
+  contentEl.appendChild(divEl)
+  inputEl.value = "";
+  setTimeout(()=>{
+    randomMessage();
 
-  contentE1.appendChild(div1);
-  inputE1.value = "";
-});
+  },2000)
+})
