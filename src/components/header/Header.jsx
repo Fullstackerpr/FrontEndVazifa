3b0322d64f9a99d39a0a7a293f8d299695c32623
Fullstackerpr <bahodirnabijanov782@gmail.com>
2025-07-28 12:14:57 +0500

    popup

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,65 @@
-import React from 'react';
-import logoo from '../../assets/logo.svg';
-import hero from '../../assets/hero.png'
+import React, { useState } from "react";
+import logoo from "../../assets/logo.svg";
+import hero from "../../assets/hero.png";
+import Popup from "../ui/Popup";
 
 const Header = () => {
-  return (
-    <header className=''>
-        <div className="container">
-            <div className='h-[25px] mt-[40px] flex justify-between'>
-                <img src={logoo} alt="" className='w-[65px] h-[26px]'/>
-                <ul className='flex size-[17px]'>
-                    <li>
-                        <span><a href="#">PROJECTS</a></span>
-                    </li>
-                    <li className='pl-[20px]'>
-                        <span><a href="#">INFO</a></span>
-                    </li>
-                </ul>
-            </div>
-            <div className='mt-[40px]'>
-                <img src={hero} alt="" className='w-[1320px] h-[806px]'/>
-            </div>
-        </div>
-    </header>
-  )
-}
+  const [show, setShow] = useState(false);
 
-export default Header
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  return (
+    <header className="">
+      <div className="container">
+        <div className="h-[25px] mt-[40px] flex justify-between">
+          <img src={logoo} alt="" className="w-[65px] h-[26px]" />
+          <ul className="flex text-[17px] items-center gap-6">
+            <li>
+              <span>
+                <a href="#" onClick={handleShow}>
+                  SIGN IN
+                </a>
+              </span>
+            </li>
+            <li>
+              <span>
+                <a href="#">INFO</a>
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <Popup isShow={show} onClose={handleClose}>
+          <div className="w-[450px] h-[400px] rounded-[10px] shadow-2xl bg-white p-4">
+            <h1 className="text-[36px] font-[600] text-center pt-[10px]">
+              Sign In
+            </h1>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full h-[50px] rounded-[5px] border-[2px] pl-[15px] border-blue-400 mt-[20px]"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full h-[50px] rounded-[5px] border-[2px] pl-[15px] border-blue-400 mt-[20px]"
+            />
+            <button className="w-full h-[50px] rounded-[5px] text-[20px] mt-[20px] bg-blue-500 text-white hover:bg-blue-400 cursor-pointer">
+              Sign In
+            </button>
+            <div className="text-center mt-[20px]">
+              <a href="#">or Sign Up?</a>
+            </div>
+          </div>
+        </Popup>
+
+        <div className="mt-[40px]">
+          <img src={hero} alt="" className="w-[1320px] h-[806px]" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/ui/Popup.jsx
+++ src/components/ui/Popup.jsx
@@ -1,10 +1,19 @@
-const Popup = () => {
-    return (
-        <>
-            <div className="fixed top-0 left-0 w-full h-screen bg-black/50"></div>
-            <div></div>
-        </>
+import React from "react";
+
+const Popup = ({ children, onClose, isShow = false }) => {
+  return (
+    isShow && (
+      <>
+        <div
+          onClick={onClose}
+          className="fixed top-0 left-0 w-full h-screen bg-black/50 z-40"
+        ></div>
+        <div className="z-50 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          {children}
+        </div>
+      </>
     )
+  );
 };
 
 export default Popup;
