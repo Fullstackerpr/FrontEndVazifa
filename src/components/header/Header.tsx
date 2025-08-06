3a767af3feec5348b928f3c3c22dccf46d81db17
Fullstackerpr <bahodirnabijanov782@gmail.com>
2025-08-06 20:05:24 +0500

    isActive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,32 @@
-import { Link } from "react-router-dom";
-import { FaUser } from 'react-icons/fa';
+import { Link, NavLink } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header>
       <div className="container bg-amber-200 pl-4 pr-4 rounded-2xl overflow-hidden">
         <div className="flex justify-between items-center  h-[60px] gap-12 text-[16px] text-gray-800">
-          <h3 className="flex-1 flex"><FaUser className="mr-2 mt-[3px]"/> User</h3>
-          <Link to={"/"} className="hover:text-blue-800 hover:underline">
+          <h3 className="flex-1 flex">
+            <FaUser className="mr-2 mt-[3px]" /> User
+          </h3>
+          <NavLink
+            className={({ isActive }) =>
+              `text-base uppercase ${isActive ? "text-blue-800 underline" : ""}`
+            }
+            to={"/"}
+          >
             Home
-          </Link>
-          <Link to={"/user"} className="hover:text-blue-800 hover:underline">
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `text-base uppercase ${isActive ? "text-blue-800 underline" : ""}`
+            }
+            to={"/user"}
+          >
             Users
-          </Link>
+          </NavLink>
         </div>
-        <div>
-        </div>
+        <div></div>
       </div>
     </header>
   );
